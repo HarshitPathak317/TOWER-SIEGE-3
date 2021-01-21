6f27e63972af5954858cee1a934c7e82e14e3b04
HarshitPathak317 <70531158+HarshitPathak317@users.noreply.github.com>
2021-01-21 19:53:35 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,48 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        //'density':1.0,
+        //'isStatic': false
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.Visiblity = 255;
+    this.score;
+    World.add(world, this.body);
+  }
+  display(){
+    
+    
+    score()
+    {
+      if (this.Visiblity < 0 && this.Visiblity > -105){
+        score++;
+      }
+    }
+
+    if(this.body.speed < 3){
+
+     }
+     else{
+       World.remove(world, this.body);
+       push();
+       this.Visiblity = this.Visiblity - 5;
+       tint(255,this.Visiblity);
+       pop();
+     }
+
+     
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(255);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
